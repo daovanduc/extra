3d24790882aec2890357f217f10e4ba083855460
Van Duc Dao <Van_Duc_Dao@epam.com>
2019-04-25 22:28:11 +0300

    added model

---
+++ srccomepamshop/model/ElectricBicycle.java
@@ -0,0 +1,8 @@
+package com.epam.dao.task1.entity;
+
+public class ElectricBicycle extends Bicycle {
+
+    public ElectricBicycle(String name, int year, int brand, double speed) {
+        super(name, year, brand, speed);
+    }
+}
