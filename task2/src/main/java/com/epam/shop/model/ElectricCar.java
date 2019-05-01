15f6689b92b505b0689b7b7e1ac35d78c79802f1
Van Duc Dao <Van_Duc_Dao@epam.com>
2019-05-01 19:47:44 +0300

    deleted model

--- task2/src/main/java/com/epam/shop/model/ElectricCar.java
+++
@@ -1,7 +0,0 @@
-package com.epam.shop.model;
-
-public class ElectricCar extends Car {
-    public ElectricCar(String name, int year, String brand, double speed) {
-        super(name, year, brand, speed);
-    }
-}
