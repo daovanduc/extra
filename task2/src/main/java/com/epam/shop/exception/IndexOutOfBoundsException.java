20894190469f3da81101c45e33b65454c1220ad6
Van Duc Dao <Van_Duc_Dao@epam.com>
2019-05-03 00:08:23 +0300

    added custom exception

---
+++ task2/src/main/java/com/epam/shop/exception/IndexOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package com.epam.shop.exception;
+
+public class IndexOutOfBoundsException extends RuntimeException {
+    public IndexOutOfBoundsException(String message) {
+        super(message);
+    }
+}
